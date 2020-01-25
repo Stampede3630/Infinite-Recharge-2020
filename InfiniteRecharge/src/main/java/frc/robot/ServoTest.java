6ef8ee5bbe1117e72ebcfa4c0e933a9a2eec8299
Matthew Sigmond <matthew28845@yandex.com>
2020-01-25 11:44:58 -0600

    Create ServoTest.java

---
+++ InfiniteRecharge/src/main/java/frc/robot/ServoTest.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package frc.robot;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Servo;
+
+/**
+ * Add your docs here.
+ */
+public class ServoTest {
+
+    Servo servo1;
+    double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+    double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+
+public ServoTest() {
+
+        servo1 = new Servo(0);
+
+        if (tv == 0)
+        {
+            ty -= 0.5;
+
+            if (ty < -1)
+            {
+                ty += 0.5;
+            }
+            
+            if (ty > 1)
+            {
+                ty -= 0.5;
+            }
+        }
+    }
+
+public void TestServo () {
+
+        servo1.set(0.5);
+    }
+
+}
