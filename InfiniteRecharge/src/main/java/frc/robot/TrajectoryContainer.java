f9dc73dc574bd746ad24accc47a5c6874645dfe5
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-01-29 19:56:07 -0600

    Update TrajectoryContainer.java

--- InfiniteRecharge/src/main/java/frc/robot/TrajectoryContainer.java
+++ InfiniteRecharge/src/main/java/frc/robot/TrajectoryContainer.java
@@ -7,16 +7,46 @@
 
 package frc.robot;
 
+import java.util.List;
+
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 
 /**
  * Add your docs here.
  */
 public class TrajectoryContainer
 {
-    PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
-    PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
-    ProfiledPIDController thetaController = new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+    Robot robot;
+    private PIDController xController = new PIDController(AutoConstants.kPXController, 0, 0);
+    private PIDController yController = new PIDController(AutoConstants.kPYController, 0, 0);
+    private Drivetrain drive = Robot.m_swerve;
+    private TrajectoryConfig config = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
+    AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+// Add kinematics to ensure max speed is actually obeyed
+.setKinematics(drive.m_kinematics);
+    
+    private ProfiledPIDController thetaController = new ProfiledPIDController(AutoConstants.kPThetaController, 0, 0, AutoConstants.kThetaControllerConstraints);
+    private Trajectory traj = TrajectoryGenerator.generateTrajectory(
+        new Pose2d(0,0, 
+        new Rotation2d(0)), 
+        List.of(
+            new Translation2d(1, 1), 
+            new Translation2d(2,-1)
+            ), 
+        new Pose2d(3,0,new Rotation2d(0)), 
+        config
+        );
 
+    
+    public TrajectoryFollowing trajectoryFollowing = new TrajectoryFollowing(drive, traj, xController, yController, thetaController);
+    
+    
 }
