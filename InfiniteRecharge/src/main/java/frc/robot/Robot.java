5a12faa082b129ecbe9c7dec311a1deef6027acf
Matthew Sigmond <matthew28845@yandex.com>
2020-01-31 16:18:13 -0600

    delete everything

--- InfiniteRecharge/src/main/java/frc/robot/Robot.java
+++ InfiniteRecharge/src/main/java/frc/robot/Robot.java
@@ -1,156 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package frc.robot;
 
-import com.ctre.phoenix.ILoopable;
-
-import edu.wpi.cscore.VideoSource;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.NetworkTableType;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import frc.robot.CANLed.*;
 
-public class Robot extends TimedRobot 
-{
- 
-  public static ShuffleboardTab tab;
-  public static DriveTrain driveTrain;
-  public static Manipulator manipulator;
-  public static Diagnostics diagnostics;
-  public static Choosers choosers;
-  public static PathChooser pathChooser = new PathChooser();
-  public static NetworkTableEntry pathSelected, hatchBallSelected, hatchLeft, hatchRight;
-  public static boolean isDeleted = false;
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
 
-  @Override
-  public void robotInit() 
-  {
-    driveTrain = new DriveTrain();
-    manipulator = new Manipulator();
-    diagnostics = new Diagnostics();
-    choosers = new Choosers(driveTrain, manipulator);
-
-    tab = Shuffleboard.getTab("driverTab");
-    pathSelected = tab.add("PathSelected", "").withWidget("PathSelector").withSize(7,6).withPosition(3, 0).getEntry();
-    hatchBallSelected = tab.add("hatchBallSelected","").withWidget("BigButtonsWidget").withSize(3,2).withPosition(0,0).getEntry();
-
-    //hatchInBoolean = tab.add("Hatch In",false).withSize(3,2).withPosition(0,3).getEntry();
-    hatchLeft = tab.add("Hatch in L", choosers.robotMap.hatchButton.get()).withPosition(11,0).withSize(1,1).withWidget(BuiltInWidgets.kBooleanBox).getEntry();
-    hatchRight = tab.add("Hatch in R", choosers.robotMap.dumbHatchButton.get()).withPosition(11,1).withSize(1,1).withWidget(BuiltInWidgets.kBooleanBox).getEntry();
-
-    for (ILoopable loop : TaskList.FullList) {
-			Schedulers.PeriodicTasks.add(loop);
-    }
-    
-
-  }
-  
-  @Override
-  public void robotPeriodic() 
-  {
-    
-    choosers.letterButtons();
-
-    diagnostics.toSmartDashboard();
-
-    pathChooser.stringToPath(pathSelected.getString(""));
-
-    diagnostics.getForwardMode();
-    
-    diagnostics.ultrasonicSensorReading(); 
-
-    Schedulers.PeriodicTasks.process();
-    
-    diagnostics.limelightValues();
-
-    choosers.updatePath();
-    
-    choosers.setManipulatorMode();
-
-    diagnostics.resetGyro();
-
-    hatchRight.setBoolean(choosers.robotMap.hatchButton.get());
-    hatchLeft.setBoolean(choosers.robotMap.dumbHatchButton.get());
-
-  }
-
-  
-  @Override
-  public void autonomousInit() 
-  {
-
-    manipulator.robotMap.ahrs.reset();
-  
-  }
-
-  
-  @Override
-  public void autonomousPeriodic() 
-  {
-
-    choosers.setDriveMode();
-
-    driveTrain.drive();
-
-    manipulator.manipulatorPeriodic();
-
-  }
-
-  @Override
-  public void teleopInit() 
-  {
-  }
-
-  @Override
-  public void teleopPeriodic() 
-  {
-
-    choosers.setDriveMode();
-
-    driveTrain.drive();
-
-    manipulator.manipulatorPeriodic();
-
-  }
-
+  private DriveTrain ;
  
   @Override
-  public void testPeriodic() 
-  {
+  public void robotInit() {
 
+    m_swerve = new DriveTrain();
+   
+  }
 
+  @Override
+  public void robotPeriodic() {
 
   }
 
   @Override
-  public void disabledInit() 
-  {
-    
+  public void autonomousInit() {
+
   }
 
-
-  //Untested
   @Override
-  public void disabledPeriodic()
-  {
-
-    SmartDashboard.putBoolean("Big Red Button", isDeleted);
-    if (isDeleted) 
-    {
-      pathSelected.delete();
-      hatchBallSelected.delete();
-
-      pathSelected = tab.add("PathSelected", "").withWidget("PathSelector").withSize(7,6).withPosition(3, 0).getEntry();
-      hatchBallSelected = tab.add("hatchBallSelected","").withWidget("BigButtonsWidget").withSize(3,2).withPosition(0,0).getEntry();
-
-      isDeleted = false;
-    }
+  public void autonomousPeriodic() {
 
   }
+
+  @Override
+  public void teleopPeriodic() {
+    m_swerve.driveWithJoystick(true);
+
+  }
+
+  @Override
+  public void testPeriodic() {
+
+  }
+
+  
 }
