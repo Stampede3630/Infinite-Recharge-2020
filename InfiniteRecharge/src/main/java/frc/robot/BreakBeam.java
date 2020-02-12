4320cf5306ed6dac34d3bf1e0d59239ad12064c9
pirtca1 <43912210+pirtca1@users.noreply.github.com>
2020-02-12 16:19:20 -0600

    breakbeams are in groups of 3 now
    Co-Authored-By: Emma Beveridge <beveem@users.noreply.github.com>
    Co-Authored-By: bagldo <bagldo@users.noreply.github.com>

--- InfiniteRecharge/src/main/java/frc/robot/BreakBeam.java
+++ InfiniteRecharge/src/main/java/frc/robot/BreakBeam.java
@@ -19,17 +19,31 @@ public class BreakBeam {
 
         beam[0] = new DigitalOutput(0);
         beam[1] = new DigitalOutput(1);
-        beam[2] = new DigitalOutput(8);
-        beam[3] = new DigitalOutput(9);
+        beam[2] = new DigitalOutput(2);
+        beam[3] = new DigitalOutput(3);
+        beam[4] = new DigitalOutput(4);
+        beam[5] = new DigitalOutput(5);
+        beam[6] = new DigitalOutput(6);
+        beam[7] = new DigitalOutput(7);
+        beam[8] = new DigitalOutput(8);
+        beam[9] = new DigitalOutput(9);
 
     }
-/**
- * @return the beam
- */
-public DigitalOutput[] getBeam() {
-    return beam;
-}
+    /**
+     * @return the beam
+     */
+    public DigitalOutput[] getBeam() {
+        return beam;
+    }
 
+    public boolean detectBall(int top, int mid, int low){
+        if(beam[top].get() && beam[mid].get() &&beam[low].get()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
 
 }
