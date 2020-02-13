6484cdff45195f55ed67fa7303eda0b3527e1956
bagldo <32798669+bagldo@users.noreply.github.com>
2020-02-13 17:30:19 -0600

    mimi and avy update trajectory but no finish
    no finsihy

--- InfiniteRecharge/src/main/java/frc/robot/StoreTrajectories.java
+++ InfiniteRecharge/src/main/java/frc/robot/StoreTrajectories.java
@@ -7,8 +7,36 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+
 /**
  * Add your docs here.
  */
 public class StoreTrajectories {
+
+    Trajectory shootpath1;
+    Trajectory shootpath2;
+
+
+   
+
+
+public StoreTrajectories() {
+//shootpath1 = TrajectoryGenerator.generateTrajectory(start, interiorWaypoints, end, config);
+ //Math.toRadians(angdeg);
+ new Pose2d(2.40, 3.05, new Rotation2d(0));
+     new Translation2d(1.69,6.166);
+     new Translation2d(1.69, 7.99);
+    
+    
+     new Translation2d(2.40,2.20);//avoid hitting pole 
+
+ )
+}
+   
+
 }
