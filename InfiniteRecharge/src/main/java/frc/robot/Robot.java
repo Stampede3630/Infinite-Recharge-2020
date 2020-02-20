3125dc63b82e8567d69f8c47203857ead24c54aa
Varghese <sam.varghese@abbott.com>
2020-02-20 12:29:52 -0600

    fixed master... finalized swerve deleted all untested unready code

--- InfiniteRecharge/src/main/java/frc/robot/Robot.java
+++ InfiniteRecharge/src/main/java/frc/robot/Robot.java
@@ -22,48 +22,22 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-<<<<<<< Updated upstream
-
-  private Drivetrain m_swerve;
-  private Compressor compp = new Compressor(0);
-  @Override
-  public void robotInit() {
-=======
   private Compressor comp = new Compressor(0);
-  private Climber climber;
 
-  private IntakeIndex ballProcessor;
-  private Shooter shoot;
-  private BreakBeam breakBeam;
   private Drivetrain m_swerve; 
-  private Chooser chooser;
-  private TrajectoryContainer tContainer;
+
   
 
   @Override
   public void robotInit() {
     m_swerve = Drivetrain.getInstance();
-    chooser = new Chooser();
-    shoot = new Shooter();
-    ballProcessor = new IntakeIndex();
-    climber = new Climber();
-    tContainer = new TrajectoryContainer();
-    SmartDashboard.putNumber("kP", 1);
-    SmartDashboard.putNumber("kF", 0.055);
-    SmartDashboard.putNumber("kI", 0);
-    SmartDashboard.putNumber("kD", 0);
-    breakBeam = BreakBeam.getInstance();
-    //BallFollowDrive.resetIntakeState();
->>>>>>> Stashed changes
-
     m_swerve = new Drivetrain();
    
   }
 
   @Override
   public void robotPeriodic() {
-    SmartDashboard.putNumber("Current trolleySpark", RobotMap.trolleySpark.getOutputCurrent());
-    SmartDashboard.putNumber("Current Elevator", RobotMap.elevatorSpark.getOutputCurrent());
+
   }
 
   @Override
@@ -74,36 +48,12 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousPeriodic() {
-<<<<<<< Updated upstream
-=======
-    tContainer.trajectoryFollowing.auto();
-    m_swerve.updateOdometry();
->>>>>>> Stashed changes
 
   }
 
   @Override
   public void teleopPeriodic() {
-<<<<<<< Updated upstream
-    //m_swerve.driveWithJoystick(true);
-    RobotMap.elevatorSpark.set(RobotMap.controller.getX(Hand.kRight) *-.8);
-    RobotMap.trolleySpark.set(RobotMap.controller.getY(Hand.kRight) *-.5);
-    System.out.println(RobotMap.controller.getY(Hand.kRight) *.5 + " , " + RobotMap.controller.getX(Hand.kRight) *.5);
-=======
     m_swerve.driveWithJoystick(true);
-    // Systemtrue.out.println(RobotMap.controller.getY(Hand.kRight) *.5 + " , " +
-    // RobotMap.controller.getX(Hand.kRight) *.5);
-    //shoot.control();
-    //ballProcessor.manualControl();
-    //ballProcessor.ToggleSolenoids();
-    climber.climberPeriodic();
-    //ballProcessor.index();
-    shoot.control();
-    ballProcessor.ToggleSolenoids();
-
-
->>>>>>> Stashed changes
-
 
   }
 
